abd9b92489d3bf1868a31a30331e8e95ea1de4cc
Henrik Silverbage <hsilverb@z4r8p1.42lyon.fr>
2023-04-28 19:29:21 +0200

    lets call it a day

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsilverb <hsilverb@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 17:52:36 by hsilverb          #+#    #+#             */
+/*   Updated: 2023/04/28 19:05:59 by hsilverb         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
@@ -16,19 +28,34 @@ typedef	struct	s_element
 typedef	struct	s_stack
 {
 	t_element	*head;
-	t_element	*tail;
+	t_element	*last;
+	int			max;
+	int			min;
 	int			size;
 }				t_stack;
 
 int	ft_check_input(int argc, char **argv);
-
-void	ft_create_list(int argc, char **argv, t_stack *stack_a);
-void	ft_fill_stack_a(int argc, char **argv, t_element *element_a, t_stack *stack_a);
-void	ft_free_stack_a(t_stack *stack);
-void	ft_rotate_a(t_stack *stack_a);
-
-int	ft_swap_a(t_stack *stack_a);
+int	ft_check_if_sorted(t_stack *stack_a);
 
 t_element	*ft_new_node(char **argv, int i);
+
+void	ft_create_list(int argc, char **argv, t_stack *stack_a);
+
+void	ft_free_stack(t_stack *stack);
+
+void	ft_rotate(t_stack *stack, char c);
+void	ft_rotate_a_and_b(t_stack *stack_a, t_stack *stack_b);
+
+void	ft_swap(t_stack *stack, char c);
+void	ft_swap_a_and_b(t_stack *stack_a, t_stack *stack_b);
+
+void	ft_reverse(t_stack *stack, char c);
+void	ft_reverse_a_and_b(t_stack *stack_a, t_stack *stack_b);
+
+void	ft_push_a(t_stack *stack_a, t_stack *stack_b);
+void	ft_push_b(t_stack *stack_a, t_stack *stack_b);
+
+void	ft_small_sort(int argc, t_stack *stack_a);
+
 
 #endif
